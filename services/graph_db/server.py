f2007a912c57d607f9cc5c3ca7081ceffe2bda9a
stefanrer <robstef85@gmail.com>
2021-12-05 20:46:35 +0300

    fix

--- services/graph_db/server.py
+++ services/graph_db/server.py
@@ -1,8 +1,8 @@
-import client
 from flask import Flask, request, jsonify
+from client import HelloWorldExample
 import logging
 
-import fill_db
+# import fill_db
 
 import json
 
@@ -16,8 +16,8 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 # fill_db.fill_db()
-greeter = client.HelloWorldExample("neo4j://localhost:7687", "neo4j", "vbifhbr!@")
-greeter.get_graph()
+greeter = HelloWorldExample("neo4j://neo4j:7687", "neo4j", "vbifhbr!@")
+# greeter.get_graph()
 stopWords = ['the', 'about', 'and', 'can', 'a', 'what', 'where', 'you', 'me', 'with', "I", "You", "He", "She", "It",
              "We", "They", "Me", "Him", "Her", "Us", "Them", "My", "Your", "Its", "Our", "Their", "Mine", "Yours",
              "His", "Hers", "Ours", "Yours", "Theirs", "Myself", "Yourself", "Himself", "Herself", "Itself",
